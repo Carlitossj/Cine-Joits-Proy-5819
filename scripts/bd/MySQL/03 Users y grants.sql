a66efc465a0ff3c7f940ddba201b2c8f9c6fd0fe
Carlitossj <alvaro.diazet12@gmail.com>
2019-11-15 11:18:36 -0300

    asd

--- scripts/bd/MySQL/03 Users y grants.sql
+++ scripts/bd/MySQL/03 Users y grants.sql
@@ -4,6 +4,7 @@ GRANT insert,select ON Pelicula TO 'administradores'@'localhost';
 GRANT insert,select ON Proyeccion TO 'administradores'@'localhost';
 GRANT insert,select ON Genero TO 'administradores'@'localhost';
 GRANT insert,select ON Sala TO 'administradores'@'localhost';
+GRANT insert,select ON Cajero TO 'administradores'@'localhost';
 
 #Punto2
 create user 'cajeros'@'10.120.0.%' identified by 'passcajeros';
