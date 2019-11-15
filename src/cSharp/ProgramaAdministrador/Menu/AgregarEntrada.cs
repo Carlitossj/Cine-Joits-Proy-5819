68d592df3edc8e27324f163d4b293860ebf29ec8
Carlitossj <alvaro.diazet12@gmail.com>
2019-11-15 10:41:26 -0300

    asd

--- src/cSharp/ProgramaAdministrador/Menu/AgregarEntrada.cs
+++ src/cSharp/ProgramaAdministrador/Menu/AgregarEntrada.cs
@@ -9,6 +9,22 @@ namespace ProgramaAdministrador.Menu
     {
         public Entrada entrada{ get; set; }
         public ListaProyecciones listaProyecciones { get; set; }
+        public ListaCajeros  listaCajeros { get; set; }
+        public override void mostrar()
+        {
+            base.mostrar();
+            Console.WriteLine();
+            var proyeccion = listaProyecciones.seleccionarElemento();
+            var fecha = Convert.ToDateTime(prompt("Ingrese fecha y hora DD / MM / AAAA HH: MMA"));
+            var cajero = listaCajeros.seleccionarElemento();
+            entrada = new Entrada()
+            {
+                Proyeccion = proyeccion,
+                FechaHora = fecha,
+                Cajero = cajero
+            };
 
+
+        }
     }
 }
---
+++ src/cSharp/ProgramaAdministrador/Menu/ListaCajeros.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MenuesConsola;
+using CineJoits1958;
+using System.Collections.Generic;
+
+namespace ProgramaAdministrador.Menu
+{
+    class ListaCajeros : MenuListador<Cajero>
+    {
+        public override void imprimirElemento(Cajero elemento)
+       => Console.WriteLine($"{elemento.dni} - {elemento.Nombre}");
+        public override List<Cajero> obtenerLista()
+            => AdoAdministrador.ADO.obtenerCajeros();
+    
+    }
+}
